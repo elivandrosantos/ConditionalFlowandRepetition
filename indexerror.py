2f16fb00c351fd8831291287e1766a9ba3c3e435
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-06 12:10:57 -0300

    Create indexerror.py

---
+++ indexerror.py
@@ -0,0 +1,13 @@
+years = [2019, 2020, 2021]
+
+current_year = years[3]
+print(current_year)
+
+# IndexError                                Traceback (most recent call last)
+# <ipython-input-18-51fedef040a1> in <cell line: 3>()
+#       1 years = [2019, 2020, 2021]
+#       2 
+# ----> 3 current_year = years[3]
+#       4 print(current_year)
+
+# IndexError: list index out of range
