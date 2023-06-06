907c3754c101cee698eb8629458da1ea1438e8f1
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-06 12:04:57 -0300

    Create exception.py

---
+++ exception.py
@@ -0,0 +1,12 @@
+price = 132.85
+people = 0
+
+value_per_person = price / people
+
+# ZeroDivisionError                         Traceback (most recent call last)
+# <ipython-input-11-3e5d09d81a47> in <cell line: 4>()
+#      2 people = 0
+#      3 
+# ---> 4 value_per_person = price / people
+
+# ZeroDivisionError: float division by zero
