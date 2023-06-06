3a0ac898da0906f0802542eb63f7a35243d582b3
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-06 12:19:54 -0300

    Create tryexceptlist.py

---
+++ tryexceptlist.py
@@ -0,0 +1,7 @@
+years = [2019, 2020, 2021]
+
+try:
+  current_year = years[3]
+  print(urrent_year)
+except Exception:
+  print('List of years is less than the chosen value. A value between 0 and 0 is expected. ' + str(len(anos) - 1))
