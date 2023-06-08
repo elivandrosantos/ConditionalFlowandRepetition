4ae55ccb110f999d78d6db5e0c416ba0e4a6dcce
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-07 23:22:46 -0300

    Create forindict_2.py

---
+++ forindict_2.py
@@ -0,0 +1,6 @@
+credit = {'123': 750, '456': 812, '789': 980}
+
+for value in credit.values():
+  print(value)
+  print(f"The credit score value is {value}, but we don't have the keys anymore :(.")
+  print('\n')
