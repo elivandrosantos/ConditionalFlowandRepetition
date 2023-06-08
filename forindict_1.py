ac87d620d94521847c94d9964072f28117569db0
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-07 23:19:27 -0300

    Update forindict_1.py

--- forindict_1.py
+++ forindict_1.py
@@ -3,5 +3,5 @@ credit = {'123': 750, '456': 812, '789': 980}
 for key in credit.keys():
   print(key)
   print(credit[key])
-  print(f'PFor the document {key}, the credit score value is {credit[key]}.')
+  print(f'For the document {key}, the credit score value is {credit[key]}.')
   print('\n')
