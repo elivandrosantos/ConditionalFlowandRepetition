bb2affb8f9ebb3796e059f1b4f34cb43a03e619b
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-20 20:27:06 -0300

    Atualizar o tryexceptlist.py

--- tryexceptlist.py
+++ tryexceptlist.py
@@ -2,6 +2,6 @@ years = [2019, 2020, 2021]
 
 try:
   current_year = years[3]
-  print(urrent_year)
+  print(current_year)
 except Exception:
-  print('List of years is less than the chosen value. A value between 0 and 0 is expected. ' + str(len(anos) - 1))
+  print('List of years is less than the chosen value. A value between 0 and 0 is expected. ' + str(len(years) - 1))
