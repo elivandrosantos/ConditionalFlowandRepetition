27a905b9453dfb921e96ea9a41a04d43018be9e1
Elivandro Santos <elivandrocsantos@gmail.com>
2023-06-20 20:25:32 -0300

    Atualizar o tryexceptdivisonbyzero.py

--- tryexceptdivisonbyzero.py
+++ tryexceptdivisonbyzero.py
@@ -5,4 +5,4 @@ try:
   value_per_people = price / people
   print(value_per_people)
 except ZeroDivisionError:
-  print('Invalid number of people. A value greater than 0 is expected and a value equal to ' + str(pessoas))
+  print('Invalid number of people. A value greater than 0 is expected and a value equal to ' + str(people))
